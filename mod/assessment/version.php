b0be5cb6264c9201c65c66cfdf4b4de7977c3043
RayhaanT <tanweer.rayhaan@gmail.com>
2020-07-14 12:43:27 -0400

    Refactor, building on quiz instead of new module

--- mod/assessment/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version   = 2020070800;
-$plugin->requires  = 2020060900;
-$plugin->component = 'mod_assessment';
--- mod/quiz/mod_form.php
+++ mod/quiz/mod_form.php
@@ -64,7 +64,7 @@ class mod_quiz_mod_form extends moodleform_mod {
         $mform = $this->_form;
 
         // -------------------------------------------------------------------------------
-        $mform->addElement('header', 'general', get_string('general', 'form'));
+        $mform->addElement('header', 'general', 'Hey bb');
 
         // Name.
         $mform->addElement('text', 'name', get_string('name'), array('size'=>'64'));
