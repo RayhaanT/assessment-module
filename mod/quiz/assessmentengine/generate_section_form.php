6a74cbf2e7ccd3bedd51c4a8e0236118d1d924d1
RayhaanT <tanweer.rayhaan@gmail.com>
2020-07-24 20:27:35 -0400

    Changed form to support submodules

--- mod/quiz/assessmentengine/generate_section_form.php
+++ mod/quiz/assessmentengine/generate_section_form.php
@@ -14,13 +14,30 @@ class generate_section_form extends moodleform {
     parent::__construct($submiturl, null, 'post', '', null, $formeditable);
   }
 
+  private function getSubModuleFields($mform, &$repeatedoptions) {
+    $repeated = array();
+    $repeated[] = $mform->createElement('text', 'topic', get_string('topic', 'quiz'));
+    $repeated[] = $mform->createElement('checkbox', 'lifecycle', get_string('lifecycleenable', 'quiz'));
+    $repeated[] = $mform->createElement('static', 'complexityqlabel', 'Number of questions of different complexities:', '');
+    $repeated[] = $mform->createElement('float', 'highq', 'High complexity');
+    $repeated[] = $mform->createElement('float', 'mediumq', 'Medium complexity');
+    $repeated[] = $mform->createElement('float', 'lowq', 'Low complexity');
+
+    $repeatedoptions['topic']['type'] = PARAM_TEXT;
+    $repeatedoptions['highq']['default'] = 0;
+    $repeatedoptions['mediumq']['default'] = 0;
+    $repeatedoptions['lowq']['default'] = 0;
+
+    return $repeated;
+  }
+
   // Define the body of the form
   protected function definition() {
     $mform = $this->_form;
 
     $mform->addElement('header', 'generalheader', get_string("general", 'form'));
 
-    // Hidden fields?
+    // Hidden fields
     $mform->addElement('hidden', 'cmid');
     $mform->setType('cmid', PARAM_INT);
 
@@ -31,32 +48,47 @@ class generate_section_form extends moodleform {
     $mform->setType('returnurl', PARAM_LOCALURL);
 
     // General fields
-    $mform->addElement('float', 'numberofquestions', get_string('numberofquestions', 'quiz'));
-    $mform->setDefault('numberofquestions', 1);
-    $mform->addRule('numberofquestions', null, 'required', null, 'client');
+    $mform->addElement('text', 'name', get_string('modulenamefield', 'quiz'));
+    $mform->setType('name', PARAM_TEXT);
+    $mform->setDefault('name', 'New Module');
 
-    // Fields used by the assessment engine to poll the question bank
-    $difficulties = array(
-      'None',
-      'Low',
-      'Medium',
-      'High'
-    );
     $roles = array(
       'None',
       'Fresher',
       'Mid-level',
       'Senior'
     );
-    $mform->addElement('checkbox', 'lifecycle', get_string('lifecycleenable', 'quiz'));
-    $mform->setDefault('lifecycle', 1);
-
-    $mform->addElement('select', 'difficulty', get_string('difficulty', 'quiz'), $difficulties);
-
-    $mform->addElement('text', 'topic', get_string('topic', 'quiz'));
-    $mform->setType('topic', PARAM_TEXT);
 
     $mform->addElement('select', 'role', get_string('role', 'quiz'), $roles);
+
+    // Submodules
+    $mform->addElement('header', 'submodulesheader', 'Submodules', '');
+    $mform->setExpanded('submodulesheader', 1);
+
+    $answersoption = '';
+    $repeatedoptions = array();
+    $repeated = $this->getSubModuleFields($mform, $repeatedoptions, $answersoption);
+    $repeatsatstart = 1;
+    $submodulesperclick = 1;
+
+    $this->repeat_elements($repeated, $repeatsatstart, $repeatedoptions,
+      'nosubmods', 'addsubmod', $submodulesperclick,
+      get_string('addsubmodule', 'quiz'), true);
+
+    // $difficulties = array(
+    //   'None',
+    //   'Low',
+    //   'Medium',
+    //   'High'
+    // );
+
+    // $mform->addElement('checkbox', 'lifecycle', get_string('lifecycleenable', 'quiz'));
+    // $mform->setDefault('lifecycle', 1);
+
+    // $mform->addElement('select', 'difficulty', get_string('difficulty', 'quiz'), $difficulties);
+
+    // $mform->addElement('text', 'topic', get_string('topic', 'quiz'));
+    // $mform->setType('topic', PARAM_TEXT);
 
     // Timing options
     $mform->addElement('header', 'timingheader', get_string('timing', 'quiz'));
--- mod/quiz/assessmentengine/generatesection.php
+++ mod/quiz/assessmentengine/generatesection.php
@@ -1,9 +1,21 @@
 <?php
 
+function addSelectCondition($currentCondition, $column, $value) {
+    if (!$value) {
+        return $currentCondition;
+    }
+    if ($currentCondition != '') {
+        $currentCondition .= ' AND ';
+    }
+    $currentCondition .= $column . " = '" . $value . "'";
+    return $currentCondition;
+}
+
 require_once(__DIR__ . '/../../../config.php');
-require_once(__DIR__ . '/../../../question/editlib.php');  
+require_once(__DIR__ . '/../../../question/editlib.php');
 require_once(__DIR__ . '/generate_section_form.php');
 require_once($CFG->libdir . '/filelib.php');
+require_once($CFG->dirroot . '/mod/quiz/locallib.php');
 require_once($CFG->libdir . '/formslib.php');
 
 // GET properties
@@ -13,23 +25,23 @@ $addbeforepage = optional_param('addbeforepage', 0, PARAM_INT);
 
 $url = new moodle_url('/mod/quiz/generatesection/generatesection.php');
 if ($returnurl !== 0) {
-  $url->param('returnurl', $returnurl);
+	$url->param('returnurl', $returnurl);
 }
 if ($cmid !== 0) {
-  $url->param('cmid', $cmid);
+	$url->param('cmid', $cmid);
 }
-if($addbeforepage !== 0) {
-  $url->param('addbeforepage', $addbeforepage);
+if ($addbeforepage !== 0) {
+	$url->param('addbeforepage', $addbeforepage);
 }
 $PAGE->set_url($url);
 
 // Validate cmid
 if ($cmid) {
-  list($module, $cm) = get_module_from_cmid($cmid);
-  require_login($cm->course, false, $cm);
-  $thiscontext = context_module::instance($cmid);
+	list($module, $cm) = get_module_from_cmid($cmid);
+	require_login($cm->course, false, $cm);
+	$thiscontext = context_module::instance($cmid);
 } else {
-  // print_error('missingcourseorcmid', 'question');
+	// print_error('missingcourseorcmid', 'question');
 }
 $contexts = new question_edit_contexts($thiscontext);
 $PAGE->set_pagelayout('admin');
@@ -47,8 +59,8 @@ $toform = new stdClass();
 $toform->returnurl = $url;
 $toform->addbeforepage = $addbeforepage;
 if ($cm !== null) {
-  $toform->cmid = $cm->id;
-  $toform->courseid = $cm->course;
+	$toform->cmid = $cm->id;
+	$toform->courseid = $cm->course;
 }
 
 $mform = new generate_section_form('generatesection.php', $contexts);
@@ -56,26 +68,198 @@ $mform->set_data($toform);
 
 // Process form data
 if ($mform->is_cancelled()) {
-  $returnurl = new moodle_url($returnurl);
-  redirect($returnurl);
-}
-else if($fromform = $mform->get_data()) {
-  $returnurl = new moodle_url('/mod/quiz/edit.php');
-  // Return data from form to quiz for processing
-  $returnurl->param('sesskey', sesskey());
-  $returnurl->param('cmid', $cmid);
+	$returnurl = new moodle_url($returnurl);
+	redirect($returnurl);
+} else if ($fromform = $mform->get_data()) {
+	$returnurl = new moodle_url('/mod/quiz/edit.php');
+	// Return data from form to quiz for processing
+	$returnurl->param('sesskey', sesskey());
+	$returnurl->param('cmid', $cmid);
 
-  // Required parameters
-  $returnurl->param('addqsection', floor($fromform->numberofquestions));
-  $returnurl->param('addbeforepage', $addbeforepage);
+	// Get quiz data objects
+	list($thispageurl, $contexts, $cmid, $cm, $quiz, $pagevars) =
+		question_edit_setup('editq', '/mod/quiz/assessmentengine/generatesection.php', true);
+	$course = $DB->get_record('course', array('id' => $quiz->course), '*', MUST_EXIST);
+	$quizobj = new quiz($quiz, $cm, $course);
+	$structure = $quizobj->get_structure();
 
-  // Optional parameters
-  if(isset($fromform->difficulty)) { $returnurl->param('difficulty', $fromform->difficulty); }
-  if(isset($fromform->role)) { $returnurl->param('role', $fromform->role); }
-  if(isset($fromform->lifecycle)) { $returnurl->param('lifecycle', $fromform->lifecycle); }
-  if(isset($fromform->topic)) { $returnurl->param('topic', $fromform->topic); }
-  if(isset($fromform->timelimit)) { $returnurl->param('timelimit', $fromform->timelimit); }
-  redirect($returnurl);
+	// Add a group of questions to the quiz based on provided parameters
+	// $structure->check_can_be_edited();
+
+	// $addbeforepage = optional_param('addbeforepage', 0, PARAM_INT);
+	// $difficulty = optional_param('difficulty', 0, PARAM_INT);
+	// $role = optional_param('role', 0, PARAM_INT);
+	// $topic = optional_param('topic', '', PARAM_ALPHA);
+	// $timelimit = optional_param('timelimit', 0, PARAM_INT);
+	// $lifecycle = optional_param('lifecycle', 0, PARAM_INT);
+	// $addqsection = optional_param('addqsection', 0, PARAM_INT);
+
+	// Get slot data for repagination
+	if ($addbeforepage == 0) {
+		$allslots = $DB->get_records('quiz_slots', array('quizid' => $quiz->id), 'slot');
+		if ($allslots) {
+			$lastslot = end($allslots)->slot;
+			$newpage = end($allslots)->page + 1;
+		} else {
+			$lastslot = 0;
+			$newpage = 1;
+		}
+	} else {
+		$pageslots = $DB->get_records('quiz_slots', array('quizid' => $quiz->id, 'page' => $addbeforepage), 'slot');
+		$lastslot = end($pageslots)->slot;
+		$newpage = $addbeforepage;
+		$numberofslots = sizeof($pageslots);
+	}
+
+	// Add questions to quiz via database and form data
+	$role = $fromform->role;
+	$addqsection = 0;
+
+	for($m = 0; $m < $fromform->nosubmods; $m++) {
+		$topic = $fromform->topic[$m];
+		if(isset($fromform->lifecycle)) {
+			$lifecycle = $fromform->lifecycle[$m];
+		} else {
+			$lifecycle = 0;
+		}
+		$highq = $fromform->highq[$m];
+		$midq = $fromform->mediumq[$m];
+		$lowq = $fromform->lowq[$m];
+
+		$condition = '';
+		$condition = addSelectCondition($condition, 'role', $role);
+		$condition = addSelectCondition($condition, 'topic', $topic);
+		if ($condition != '' && $lifecycle) {
+			$condition .= ' AND ';
+		}
+		if ($lifecycle) {
+			$condition .= '(lifecycleexpiry > ' . time() . ' OR lifecycleexpiry = 0)';
+		}
+		if ($condition != '') {
+			$condition .= ' AND ';
+		}
+		$condition .= 'parent = 0';
+
+		if($highq) {
+			$highcondition= addSelectCondition($condition, 'difficulty', 3);
+			$qpool = $DB->get_records_select('question', $highcondition);
+			$maxindex = sizeof($qpool) - 1;
+			if ($maxindex + 1 < $highq) {
+				$highq = $maxindex + 1;
+			}
+			$indexedpool = [];
+			foreach ($qpool as $q) {
+				array_push($indexedpool, $q);
+			}
+			for ($y = 0; $y < $highq; $y++) {
+				$newq = rand(0, $maxindex);
+				quiz_add_quiz_question($indexedpool[$newq]->id, $quiz, $addbeforepage);
+				array_splice($indexedpool, $newq, 1);
+				$maxindex--;
+			}
+		} else {$highq = 0;}
+
+		if ($midq) {
+			$midcondition = addSelectCondition($condition, 'difficulty', 2);
+			$qpool = $DB->get_records_select('question', $midcondition);
+			$maxindex = sizeof($qpool) - 1;
+			if ($maxindex + 1 < $midq) {
+				$midq = $maxindex + 1;
+			}
+			$indexedpool = [];
+			foreach ($qpool as $q) {
+				array_push($indexedpool, $q);
+			}
+			for ($y = 0; $y < $midq; $y++) {
+				$newq = rand(0, $maxindex);
+				quiz_add_quiz_question($indexedpool[$newq]->id, $quiz, $addbeforepage);
+				array_splice($indexedpool, $newq, 1);
+				$maxindex--;
+			}
+		} else {$midq = 0;}
+
+		if ($lowq) {
+			$lowcondition = addSelectCondition($condition, 'difficulty', 1);
+			$qpool = $DB->get_records_select('question', $lowcondition);
+			$maxindex = sizeof($qpool) - 1;
+			if ($maxindex + 1 < $lowq) {
+				$lowq = $maxindex + 1;
+			}
+			$indexedpool = [];
+			foreach ($qpool as $q) {
+				array_push($indexedpool, $q);
+			}
+			for ($y = 0; $y < $lowq; $y++) {
+				$newq = rand(0, $maxindex);
+				quiz_add_quiz_question($indexedpool[$newq]->id, $quiz, $addbeforepage);
+				array_splice($indexedpool, $newq, 1);
+				$maxindex--;
+			}
+		} else {
+			$lowq = 0;
+		}
+
+		$addqsection += $lowq + $midq + $highq;
+	}
+
+	// Repaginate to place all questions from section on same page
+	$repage = new \mod_quiz\repaginate($quiz->id);
+	if ($addbeforepage == 0) {
+		// Close page gaps between added questions
+		for ($x = 0; $x < $addqsection - 1; $x++) {
+			$repage->repaginate_slots($lastslot + $x + 2, 1);
+		}
+	} else {
+		// Move all questions from later pages down a page
+		$condition = "page > '" . $addbeforepage . "'" . " AND " . "quizid = '" . $quiz->id . "'";
+		$allslots = $DB->get_records_select('quiz_slots', $condition);
+		foreach ($allslots as $s) {
+			$s->page = $s->page + 1;
+			$DB->update_record('quiz_slots', $s);
+		}
+
+		// Move new slots out of the way to prevent id conflicts
+		$allpageslots = $DB->get_records('quiz_slots', array('quizid' => $quiz->id, 'page' => $addbeforepage), 'slot');
+		$placeholderslot = -1;
+		$oldslotnumbers = [];
+		foreach ($allpageslots as $s) {
+			if ($s->slot > $lastslot) {
+				array_push($oldslotnumbers, $s->slot);
+				$s->slot = $placeholderslot;
+				$placeholderslot--;
+				$DB->update_record('quiz_slots', $s);
+			}
+		}
+
+		$indexedpageslots = [];
+		foreach ($pageslots as $s) {
+			array_push($indexedpageslots, $s);
+		}
+		array_reverse($indexedpageslots);
+		// Move all questions on current page to next page
+		foreach ($pageslots as $s) {
+			$s->page = $s->page + 1;
+			$s->slot = $s->slot + $addqsection;
+			$DB->update_record('quiz_slots', $s);
+		}
+
+		$allpageslots = $DB->get_records('quiz_slots', array('quizid' => $quiz->id, 'page' => $addbeforepage), 'slot');
+		$indexedslots = [];
+		foreach ($allpageslots as $x) {
+			array_push($indexedslots, $x);
+		}
+		// Move new slots back into place
+		for ($x = 0; $x < sizeof($indexedslots); $x++) {
+			$indexedslots[$x]->slot = $oldslotnumbers[$x] - sizeof($pageslots);
+			$DB->update_record('quiz_slots', $indexedslots[$x]);
+		}
+	}
+
+	// Wrap up
+	quiz_delete_previews($quiz);
+	quiz_update_sumgrades($quiz);
+
+	redirect($returnurl);
 }
 
 $PAGE->set_title('Section generation');
